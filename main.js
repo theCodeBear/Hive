fdef078a1ba937f4ba1c90e094daedea720b8510
Todd Kronenberg <toddkronenberg@gmail.com>
2015-03-29 22:36:08 -0700

    Highlight piece on hover, adjacent pieces on click

--- main.js
+++ main.js
@@ -12,26 +12,80 @@ $(function() {
         evenClass = ' even'
       $hexRow.append('<div class="hex' + evenClass + '" id="' + row + col + '">' +
                      '<div class="hexLeft"></div>' +
-                     '<div class="hexCenter"></div>' +
+                     '<div class="hexCenter">' + row + col + '</div>' +
                      '<div class="hexRight"></div>' +
                      '</div>');
     }
     $('#gameBoard').append($hexRow);
   }
 
-  colorHex('11', 'orange');
-  for (var i=0; i<6; i++) {
-    
-  }
+  // colorHex('11', 'orange');
+
+  $('.hex').on('click', function() {
+    var id = $(this).attr('id');
+    highlightSurrounding(id);
+  });
 
 });
 
+function highlightSurrounding(id) {
+  if (id % 2 === 1)
+    highlightForOddId(id);
+  else
+    highlightForEvenId(id);
+}
+
+function highlightForOddId(id) {
+  var row = id.split('')[0];
+  var col = id.split('')[1];
+  colorHex((parseInt(row)-1) + col, 'blue');    // up
+  colorHex((parseInt(row)+1) + col, 'blue');    // down
+  colorHex(row + (parseInt(col)-1), 'blue');    // left-up
+  colorHex(row + (parseInt(col)+1), 'blue');    // right-up
+  colorHex((parseInt(row)+1) + '' + (parseInt(col)-1), 'blue');     // left-down
+  colorHex((parseInt(row)+1) + '' + (parseInt(col)+1), 'blue');     // right-down
+}
+
+function highlightForEvenId(id) {
+  var row = id.split('')[0];
+  var col = id.split('')[1];
+  colorHex((parseInt(row)-1) + col, 'blue');    // up
+  colorHex((parseInt(row)+1) + col, 'blue');    // down
+  colorHex((parseInt(row)-1) + '' + (parseInt(col)-1), 'blue');     // left-down
+  colorHex((parseInt(row)-1) + '' + (parseInt(col)+1), 'blue');     // right-down
+  colorHex(row + (parseInt(col)-1), 'blue');     // left-down
+  colorHex(row + (parseInt(col)+1), 'blue');     // right-down
+}
 
 function colorHex(id, color) {
+  // $('.highlight').removeClass('highlight');
+  // $('#' + id).addClass('highlight');
   $('#' + id + ' .hexLeft').css('border-right-color', color);
   $('#' + id + ' .hexCenter').css('background-color', color);
   $('#' + id + ' .hexRight').css('border-left-color', color);
 }
+
+// .hexLeft:hover {
+//   /* height: 100px; */
+//   float: left;
+//   border-top: 52px solid transparent;
+//   border-bottom: 52px solid transparent;
+//   border-right: 30px solid yellow;
+// }
+
+// .hexCenter:hover {
+//   float: left;
+//   width: 60px;
+//   height: 104px;
+//   background-color: yellow;
+// }
+
+// .hexRight:hover {
+//   float: left;
+//   border-top: 52px solid transparent;
+//   border-bottom: 52px solid transparent;
+//   border-left: 30px solid yellow;
+// }
 
 
 // <div class='hex-row'>
